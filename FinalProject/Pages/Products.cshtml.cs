ee5e56fa37fe4884535bd05683d808be7a7677ac
annfont0 <fontanillajasmine00@gmail.com>
2022-11-21 05:37:20 +0800

    Nov. 21 -

--- FinalProject/Pages/Products.cshtml.cs
+++ FinalProject/Pages/Products.cshtml.cs
@@ -1,3 +1,4 @@
+using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace FinalProject.Pages
 {
     public class ProductsModel : PageModel
     {
+        readonly IConfiguration _configuration;
+
+        public List<Products> products = new List<Products>();
+
+        string connectionString;
+
+        public ProductsModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            products = GetProductList();
+        }
+
+        private List<Products> GetProductList()
+        {
+            connectionString = _configuration.GetConnectionString("ConnectionString");
+            List<Products> ProductList = new List<Products>();
+            Products products = new Products();
+            ProductList = products.GetProducts(connectionString);
+            return ProductList;
         }
     }
 }
