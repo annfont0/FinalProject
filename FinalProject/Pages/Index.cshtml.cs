b1a22ef209f71ba8fac51c3baf26ecc47c44da72
annfont0 <fontanillajasmine00@gmail.com>
2022-11-21 04:11:03 +0800

    Nov. 21 - Index; Getting Products data from database

--- FinalProject/Pages/Index.cshtml.cs
+++ FinalProject/Pages/Index.cshtml.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data.SqlClient;
+using FinalProject.Models;
 
 namespace FinalProject.Pages
 {
     public class IndexModel : PageModel
     {
+        readonly IConfiguration _configuration;
+
+        public List<Products> products = new List<Products>();
+
+        string connectionString;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            products = GetProductList();
+        }
+
+        private List<Products> GetProductList()
+        {
+            connectionString = _configuration.GetConnectionString("ConnectionString");
+            List<Products> ProductList = new List<Products>();
+            Products products = new Products();
+            ProductList = products.GetProducts(connectionString);
+            return ProductList;
         }
     }
 }
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+startup.Configure(app, builder.Environment);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
