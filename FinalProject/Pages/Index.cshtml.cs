9e06b094f77d816f31cbd850f94905c75688beab
annfont0 <fontanillajasmine00@gmail.com>
2022-11-20 09:49:58 +0800

    Add project files.

---
+++ FinalProject/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace FinalProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
